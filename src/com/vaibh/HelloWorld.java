9a8a1152c3e59bdb72ead6fcd1a640cd96c6a660
VaibhSi <vsc567890@gmail.com>
2025-07-11 13:39:39 +0530

    c2

--- src/com/vaibh/HelloWorld.java
+++ src/com/vaibh/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test code");
+		System.out.println("Test code2");
 	}
 
 }
